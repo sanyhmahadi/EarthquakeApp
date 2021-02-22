fb7d6f1c1861b0809c9a8f110c85b3d0e633783b
Your Name <you@example.com>
2021-02-22 20:38:11 +0600

    Add the google map

--- lib/earthquake_app/show_map.dart
+++ lib/earthquake_app/show_map.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class ShowMap extends StatefulWidget {
   @override
@@ -7,8 +8,21 @@ class ShowMap extends StatefulWidget {
 
 class _ShowMapState extends State<ShowMap> {
   GoogleMapController mapController;
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+  void onMapController(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Map"),
+        centerTitle: true,
+      ),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(target: _center, zoom: 15),
+      ),
+    );
   }
 }
